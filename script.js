d612ae950775f4d57c5f8695ce93213be7eff01a
Davit Tkeshelashvili <dtkeshelashvili07@gmail.com>
2023-10-07 21:19:15 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('submitBtn').addEventListener('click', function() {
+    const anchor = document.getElementById('map');
+    anchor.scrollIntoView({ behavior: 'smooth' });
+});
